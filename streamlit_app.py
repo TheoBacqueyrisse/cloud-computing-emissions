961ca65bf142c5ebcccfbd9dab13fb002eea396e
TheoBacqueyrisse <144768551+TheoBacqueyrisse@users.noreply.github.com>
2023-12-21 11:44:38 +0000

    continue app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,28 @@
 import pandas as pd
 import numpy as np
-import requests
-import os
-import json
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import requests
+# import os
+# import json
 import streamlit as st
-import subprocess
+# import subprocess
+
+custom_css = """
+.css-1l02zno {
+    padding-left: 15px !important;
+    padding-right: 15px !important;
+}
+"""
+
+# Inject custom CSS
+st.markdown(f'<style>{custom_css}</style>', unsafe_allow_html=True)
 
 st.title("Carbon Emissions from Cloud Computing")
 st.write("Hello, this is a Streamlit app for our Data Mining Project")
 st.write("We are two students of the M2 - D3S at TSE")
+
+st.markdown("<hr>", unsafe_allow_html=True)
 
 # SideBar Configuration
 
@@ -29,23 +43,39 @@ selected_option = st.sidebar.selectbox('Sélectionnez une option', options)
 if selected_option == 'Tidyverse':
   df_jobs_with_co2 = pd.read_csv('/content/cloud_computing_emissions/0_Data/tidyverse_data.csv')
 
-sum_em_repo = df_jobs_with_co2['co2_emission'].sum()
-sum_em_repo = "{:.3f}".format(sum_em_repo)
 
-col1, col2 = st.beta_columns(2)
+if selected_option:
 
-# Content for the first column
-with col1:
-    st.header(f"Global KPI for the {selected_option} repository")
-    st.write(f'Total Co2 emissions : {sum_em_repo}') 
-    st.write(f'Number of Runs : {len(df_jobs['run_id'].unique())}')
-    st.write(f'Number of Jobs : {len(df_jobs)}')
-    st.write(f'Mean Number of Jobs per Run : {len(df_jobs) // len(df_jobs['run_id'].unique())}')
+  sum_em_repo = df_jobs_with_co2['co2_emission'].sum()
+  sum_em_repo = "{:.3f}".format(sum_em_repo)
 
-# Content for the second column
-with col2:
-    st.header("Co2 Emissions across Time")
-    plt.figure(figsize=(10, 6))
-    plt.plot(df_jobs[''], df_jobs['co2_emission'], color='r')
-    plt.xlabel('Date')
-    plt.ylabel('Co2 Emission')
+  nb_runs = len(df_jobs_with_co2['run_id'].unique())
+
+  nb_jobs = len(df_jobs_with_co2)
+
+  mean_jobs_per_run = len(df_jobs_with_co2) // len(df_jobs_with_co2['run_id'].unique())
+
+
+  col1, col2 = st.columns(2)
+
+  # Content for the first column
+  with col1:
+      st.write(f"**Global KPI for the {selected_option} repository**")
+      st.write(f'Total Co2 emissions : {sum_em_repo}') 
+      st.write(f'Number of Runs : {nb_runs}')
+      st.write(f'Number of Jobs : {nb_jobs}')
+      st.write(f'Mean Number of Jobs per Run : {mean_jobs_per_run}')
+
+  # Content for the second column
+  with col2:
+      st.write(f"**Carbon Emissions across time**")
+
+      x = df_jobs_with_co2['started_at']
+      y = df_jobs_with_co2['co2_emission']
+
+      sns.set_style("whitegrid")
+      fig, ax = plt.subplots()
+      ax.plot(x, y)
+      ax.set_xlabel('Date')
+      ax.set_ylabel('Co2 Emission')
+      st.pyplot(fig)
